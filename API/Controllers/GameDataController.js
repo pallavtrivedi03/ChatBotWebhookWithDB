fc672697dc9745f1c7100b1839a1361ef0b3f19b
Pallav <pallavtrivedi03@gmail.com>
2018-02-14 18:35:02 +0530

    previous game functionality added

--- API/Controllers/GameDataController.js
+++ API/Controllers/GameDataController.js
@@ -71,12 +71,88 @@ function getTeamSchedule(req,res)
   //   if(err)	return err;
   // });
 
+  let parameters = req.body.result.parameters;
+  if (parameters.team1 == "")
+  {
+    let game_occurence = parameters.game_occurence;
+    let team = parameters.team;
+    if (game_occurence == "previous")
+    {
+      //previous game
+      GameSchedule.find({opponent:team},function(err,games)
+      {
+        if (err)
+        {
           return res.json({
-              speech: 'This module is under development. Schedule will be available soon',
-              displayText: 'This module is under development. Schedule will be available soon',
+              speech: 'Something went wrong!',
+              displayText: 'Something went wrong!',
               source: 'game schedule'
           });
+        }
+        if (games)
+        {
+          var requiredGame;
+          for (var i=0; i < games.length; i++)
+          {
+              var game = games[i];
 
+              var convertedCurrentDate = new Date();
+              var convertedGameDate = new Date(game.date);
+              console.log('converted game date is -------> '+convertedGameDate);
+              console.log('converted current date is -------> '+convertedCurrentDate);
+
+              if (convertedGameDate > convertedCurrentDate)
+              {
+                if(games.length > 1)
+                {
+                  requiredGame = games[i-1];
+
+                  var winningStatement = "";
+                  if (requiredGame.isWinner)
+                  {
+                      winningStatement = "Kings won this match by "+requiredGame.score;
+                  }
+                  else {
+                    winningStatement = "Kings lost this match by "+requiredGame.score;
+                  }
+                  return res.json({
+                      speech: 'Last game between Kings and '+parameters.team+' was played on '+requiredGame.date+' .'+winningStatement,
+                      displayText: 'Last game between Kings and '+parameters.team+' was played on '+requiredGame.date+' .'+winningStatement,
+                      source: 'game schedule'
+                  });
+                  break;
+                }
+                else {
+                  return res.json({
+                      speech: 'Cant find any previous game played between Kings and '+parameters.team,
+                      displayText: 'Cant find any previous game played between Kings and '+parameters.team,
+                      source: 'game schedule'
+                  });
+                }
+              }
+          }
+
+
+
+        }
+
+      });
+    }
+    else {
+      return res.json({
+          speech: 'Next game schedules will be available soon',
+          displayText: 'Next game schedules will be available soon',
+          source: 'game schedule'
+      });
+    }
+  }
+  else {
+    return res.json({
+        speech: 'Cant handle the queries with two teams now. I will update myself',
+        displayText: 'Cant handle the queries with two teams now. I will update myself',
+        source: 'game schedule'
+    });
+  }
 }
   //let reqUrl = encodeURI('http://theapache64.xyz:8080/movie_db/search?keyword=' + movieToSearch);
   /*http.get(reqUrl, (responseFromAPI) => {
